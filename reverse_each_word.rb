0efbb07724eabccb3d605d5584a24c5fb5aed12a
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 05:15:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word
 string = "Hello there, and how are you?"
-string.split(" ")
+cool_array = string.split(" ")
 new_array = []
-string.each do |l|
+cool_array.each do |l|
 new_array << l.reverse
 end
 new_array.join(" ")
